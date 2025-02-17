223045c7d3ff23d1704ec2599a462c04cbc267f8
root <lapique.philippe@gmail.com>
2025-02-17 14:16:58 +0000

    plouf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main-Entry point
+*Description:Affiche du texte a l'ecran
+*Printf:commande
+*Return:Retourne a la ligne
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,/n");
+return (0);
+}
