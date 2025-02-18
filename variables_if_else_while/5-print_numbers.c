9511c1bd9280e1a2a3b5db8de9cee78d83276870
root <lapique.philippe@gmail.com>
2025-02-18 15:32:40 +0000

    plouf

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int chiffre;
+
+	for (chiffre = 0 ; chiffre <= 9 ;  chiffre ++)
+	{
+	printf("%d", chiffre);
+	}
+	return (0);
+}
