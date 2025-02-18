44e966221f874bd1ee89d9a52cf7d1036270bc69
root <lapique.philippe@gmail.com>
2025-02-18 12:53:19 +0000

    plouf

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int putc(int c, FILE *stream);
+	int putchar(int c);
+
+	{
+	putchar("{a.z}\n");
+	}
+	return (0);
+}
+
