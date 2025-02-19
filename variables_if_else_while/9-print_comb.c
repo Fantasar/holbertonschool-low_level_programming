58d500364b48227e061efad863606ee9d37e2ca5
root <lapique.philippe@gmail.com>
2025-02-19 13:38:25 +0000

    plouf

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+	putchar(x + '0');
+	if (x < 9)
+		{
+			putchar(',');
+        		putchar(' ');
+        	}
+	}
+	putchar('\n');
+	return (0);
+}
