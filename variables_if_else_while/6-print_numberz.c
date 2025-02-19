025e5b1b7e2b957ef619f6c0351f5d1fe2fce80f
root <lapique.philippe@gmail.com>
2025-02-19 08:58:22 +0000

    plouf

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0
  */
+
 int main(void)
+
 {
 	int chiffre;
 
-	for (chiffre = 0 ;  chiffre < 20 ; chiffre++)
+	for (chiffre = 0 ;  chiffre < 10 ; chiffre++)
 		putchar((chiffre % 10 ) + '0');
 
 	putchar('\n');
