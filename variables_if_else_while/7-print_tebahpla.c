6bf34f81f654f3084adfeebbf550f2bbf6373516
root <lapique.philippe@gmail.com>
2025-02-19 09:50:01 +0000

    plouf

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -11,12 +11,12 @@ int main(void)
 
 	int alph;
 
-	for(alph = 122; alph >= 97; alph --)
+	for (alph = 122; alph >= 97; alph --)
 		{
 		putchar (alph);
 		}
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
