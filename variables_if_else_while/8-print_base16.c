11263a53bb71f4e428c35c72b8cca8bfcc79d5b7
root <lapique.philippe@gmail.com>
2025-02-19 11:30:16 +0000

    plouf

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	int cheval; 
+	char alphabet;
+
+	for(cheval  = 0 ; cheval <= 9 ; cheval++)
+	{
+	putchar((cheval % 10)+ '0');
+	}
+
+	for (alphabet = 'a' ; alphabet <= 'f' ; alphabet++)
+	{
+	putchar(alphabet);
+	}
+
+	putchar ('\n');
+
+	return(0);
+}
