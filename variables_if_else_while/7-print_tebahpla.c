f2f0e7aef1666942a8730a677861f51079536a69
root <lapique.philippe@gmail.com>
2025-02-19 09:48:02 +0000

    plouf

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	int alph;
+
+	for(alph = 122; alph >= 97; alph --)
+		{
+		putchar (alph);
+		}
+
+	putchar('\n');
+
+	return(0);
+}
