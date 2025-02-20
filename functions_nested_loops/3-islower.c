9f11a59845bfa43834d3fdfe082f33c0952cb789
root <lapique.philippe@gmail.com>
2025-02-20 16:52:09 +0000

    plouf

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * int _islower(int c) - Prints the alphabet 10 times,
+ *		each time followed by a new line.
+ *
+ * Description: This function prints the lowercase alphabet 10 times,
+ *              each time followed by a new line character.
+ *
+ * Return: void
+ */
+int _islower(int c)
+
+{
+	char c ='a';
+
+	if (isower(c))
+	{
+	_putchar(1);
+	}
+	else (isower(c))
+	{
+	_putchar(0);
+	}
+
+	return(0);
+}
