5dbeb4861680913f77ecca50fb93b582d1a80f2e
root <lapique.philippe@gmail.com>
2025-02-20 15:52:42 +0000

    plouf

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -9,13 +9,12 @@
 int print_alphabet(void)
 
 {
-	int b;
-	{
-		for (b ='a';b <= 'z' ; b++)
-			{
-			_putchar(b);
-			_putchar('\n');
-			}
-	}
+	char b;
+
+	for (b = 'a';b <= 'z' ; b++)
+		{
+		_putchar(b);
+		}
+		_putchar('\n');
 	return(0);
 }
