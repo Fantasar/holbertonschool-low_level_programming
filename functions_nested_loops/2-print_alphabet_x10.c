f54f6beab79e67fa0734d9989e1474964f30552e
root <lapique.philippe@gmail.com>
2025-02-20 16:33:58 +0000

    plouf

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times, 
+ * print_alphabet_x10 - Prints the alphabet 10 times,
  *		each time followed by a new line.
  *
  * Description: This function prints the lowercase alphabet 10 times,
