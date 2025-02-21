ed3443dc5d6e1f773601d98c4c397e53a184c8e4
root <lapique.philippe@gmail.com>
2025-02-21 11:16:32 +0000

    plouf

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *_abs - Verifie si un caractere est une lettre minuscule
+ *@n: Le caractere aÃverifier
+ *
+ *Cette fonction prend un caractere en entre et retourne 1 si
+ *le caractere est une lettre minuscule
+ *(comprise entre 'a' et 'z' en termes de valeurs ASCII).
+ *Sinon, elle retourne 0.
+ *
+ *Return:
+ *1 si le caractere est une lettre minuscule, 0 sinon.
+ */
+int _abs(int n)
+{
+	if(n < 0)
+	{
+	return (-n);
+	}
+	return n;
+}
