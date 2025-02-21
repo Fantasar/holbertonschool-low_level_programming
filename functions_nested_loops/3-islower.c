2965670a5b59555792b4237b59ed2e0f4bfe063a
root <lapique.philippe@gmail.com>
2025-02-21 09:28:31 +0000

    plouf

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,17 +4,19 @@
 /**
  * int_islower(int c) - Prints the alphabet 10 times,
  *		each time followed by a new line.
+ * _islower - Verifie si un caractere est une lettre minuscule.
+ * @c: Le caractere a  verifier.
  *
+ * Retourne : 1 si le caractère est une lettre minuscule, 0 sinon
  * Description: This function prints the lowercase alphabet 10 times,
  *              each time followed by a new line character.
  *
- * Return: void
  */
 int _islower(int c)
 {
-if (c >= 'a'&& c<= 'z')
-return(1);
-else 
+if (c >= 'a' && c <= 'z')
+return (1);
+else
 return (0);
 
 }
