2ad584abc43819c408751916b373fbb2d597238b
root <lapique.philippe@gmail.com>
2025-02-21 14:35:18 +0000

    plouf

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ *void jack_bauer - Verifie si un caractere est une lettre minuscule
+ *@n: Le caractere aÃverifier
+ *
+ *Cette fonction prend un caractere en entre et retourne 1 si
+ *le caractere est une lettre minuscule
+ *(comprise entre 'a' et 'z' en termes de valeurs ASCII).
+ *Sinon, elle retourne 0.
+ *
+ *Return:
+ *1 si le caractere est une lettre minuscule, 0 sinon.
+ */
+
+void jack_bauer(void)
