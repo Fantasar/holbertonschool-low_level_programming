72bf79323bf72b2870d2e673e366099107a0cd09
root <lapique.philippe@gmail.com>
2025-02-21 10:02:05 +0000

    plouf

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *_islower - Verifie si un caractere est une lettre minuscule
+ *@c: Le caractere aÃverifier
+ *
+ *Cette fonction prend un caractere en entre et retourne 1 si
+ *le caractere est une lettre minuscule
+ *(comprise entre 'a' et 'z' en termes de valeurs ASCII).
+ *Sinon, elle retourne 0.
+ *
+ *Return:
+ *1 si le caractere est une lettre minuscule, 0 sinon.
+ */
+
+int _isalpha(int c)
+{
+
+if ((c >= 'a' && c <= 'z') || (c>= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int print_alphabet();
 int main(void);
 int print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
