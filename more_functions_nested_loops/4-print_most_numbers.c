cc22fac8da88dee5187a0b191498e8fba560e5db
root <lapique.philippe@gmail.com>
2025-02-24 12:45:18 +0000

    plouf

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - multiplies two integers
+ * @a char
+ *
+ * Return: 0.
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = 0; a <= 9; a++)
+		{
+			if (a != 2 )
+				{
+				if (a != 4 )
+				_putchar ((a % 10) + '0');
+				}
+		}
+		_putchar('\n');
+}
