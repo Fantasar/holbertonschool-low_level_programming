c4062f843b9a45acda2ffbd74112702e05329bdb
root <lapique.philippe@gmail.com>
2025-02-24 12:34:05 +0000

    plouf

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int a
+ * @b: int b
+ *
+ *
+ * Return: 0.
+ */
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 0; a <= 9; a++)
+		{
+		_putchar ((a % 10) + '0');
+		}
+		_putchar('\n');
+}
