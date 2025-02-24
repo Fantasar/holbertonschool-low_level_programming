2a0e7b966252ad89fac68b068a137d9829cd6bc6
root <lapique.philippe@gmail.com>
2025-02-24 15:12:20 +0000

    plouf

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - multiplies two integers
+ *
+ *
+ * Return: 0.
+Ã**/
+void print_line(int n)
+{
+	int a;
+
+	for ( a = 0; a < n; a++)
+	{
+	_putchar ('_');
+	}
+	_putchar ('\n');
+}
