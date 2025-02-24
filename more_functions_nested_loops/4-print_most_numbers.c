fb54d44d18ddf707a3179fce28e9c9bc83b586f1
root <lapique.philippe@gmail.com>
2025-02-24 12:50:11 +0000

    plouf

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,17 +6,18 @@
  *
  * Return: 0.
  */
+
 void print_most_numbers(void)
 {
 	char a;
 
 	for (a = 0; a <= 9; a++)
 		{
-			if (a != 2)
-				{
-				if (a != 4)
-				_putchar ((a % 10) + '0');
-				}
+	if (a != 2)
+		{
+	if (a != 4)
+		_putchar ((a % 10) + '0');
 		}
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
