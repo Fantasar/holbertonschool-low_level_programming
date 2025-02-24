a20de4099c2d788dae89708133c23fbbdea04083
root <lapique.philippe@gmail.com>
2025-02-24 11:39:03 +0000

    plouf

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int a
+ * @b: int b
+ *
+ *
+ * Return: 0.
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -9,6 +9,7 @@ int _abs(int);
 int print_last_digit(int);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 void jack_bauer(void);
 void times_table(void);
