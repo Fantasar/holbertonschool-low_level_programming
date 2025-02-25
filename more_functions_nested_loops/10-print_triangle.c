7ee8a9d7681557c4edcce1f33c22933f4ef66514
root <lapique.philippe@gmail.com>
2025-02-25 16:38:51 +0000

    plouf

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,18 +11,18 @@ void print_triangle(int size)
 	int a;
 	int b;
 
-	if(size <= 0)
+	if (size <= 0)
 	{
 	_putchar('\n');
 	return;
 	}
-	for(b = 1; b <= size; b++)
+	for (b = 1; b <= size; b++)
 	{
-	for(a = size - b; a > 0; a--)
+	for (a = size - b; a > 0; a--)
 	{
 	_putchar(' ');
 	}
-	for(a = 1; a <= b; a++)
+	for (a = 1; a <= b; a++)
 	{
 	_putchar('#');
 	}
