3170b48ea053a51088a64e3bfbbbda29b297edad
root <lapique.philippe@gmail.com>
2025-02-25 16:37:40 +0000

    plouf

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,32 @@
 #include "main.h"
 
 /**
- * print_square - multiplies two integers
+ * print_triangle - multiplies two integers
  *@size: int size
  *
- * Return: 0.
+ * Return: void.
  **/
 void print_triangle(int size)
 {
 	int a;
 	int b;
 
-	if (size <= 0)
+	if(size <= 0)
 	{
 	_putchar('\n');
+	return;
 	}
-	for (b = 1; b <= size; b++)
-		{
-		for (a = 1; a <= b; a++)
-		{
-		_putchar('#');
-		}
-		_putchar('\n');
+	for(b = 1; b <= size; b++)
+	{
+	for(a = size - b; a > 0; a--)
+	{
+	_putchar(' ');
+	}
+	for(a = 1; a <= b; a++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
 	}
 }
 
