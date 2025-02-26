72c7ffab4abb62c72522538fa08e28d3f7d74f3b
root <lapique.philippe@gmail.com>
2025-02-26 08:27:32 +0000

    plouf

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *swap_int - multiplies two integers
+ *@a: int a
+ *@b: int b
+ * Return: 0.
+**/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
