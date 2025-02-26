3ed1eabf4afaa4d26678036d405b1432a37ba8b0
root <lapique.philippe@gmail.com>
2025-02-26 14:40:17 +0000

    plouf

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,23 @@
 #include "main.h"
 
 /**
- *_puts - multiplies two integers
- *@str: int size
- *
+ *_print_rev - multiplies two integers
+ *@Plouf: int size
+ *@i : int
  * Return: void.
  **/
 void print_rev(char *s)
 {
-while (
+	int Plouf = 0;
+	int i;
 
-
+	while(s[Plouf] != '\0')
+		{
+		Plouf++;
+		}
+	for(i = Plouf - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
