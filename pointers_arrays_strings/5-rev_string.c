2234febdd92afd2ee8fe341b02945c593b5f3e10
root <lapique.philippe@gmail.com>
2025-02-27 18:18:45 +0000

    plouf

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*rev_string - plouf
+*
+*@s: rrr
+*Return: Always 0
+**/
+void rev_string(char *s)
+{
+int plouf = 0;
+int i;
+char temp;
+for (plouf = 0; s[plouf] != '\0'; plouf++)
+{
+}
+for (i = 0; i < plouf / 2 ; i++)
+{
+temp = s[i];
+s[i] = s[plouf - i - 1];
+s[plouf - i - 1] = temp;
+}
+}
