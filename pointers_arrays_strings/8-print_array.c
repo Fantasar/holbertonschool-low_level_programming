749022d1fe9f57e59dfd0891b5fa4a91ea19cb83
root <lapique.philippe@gmail.com>
2025-02-28 10:09:43 +0000

    plouf

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+*print_array - plouf
+*@str : plouf
+*Return: Always 0
+**/
+void print_array(int *a, int n)
+{
+int b;
+	{
+		if (n <= 0)
+		{
+		return;
+		}
+		for (b = 0; b < n ; b++)
+		{
+		if ( b == 0 )
+		{
+		printf("%d", a[b]);
+		}
+		else
+		printf(",Ã%d",a[b]);
+		}
+	}
+	printf("\n");
+}
