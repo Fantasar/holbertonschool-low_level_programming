e9191867c9426ef377fc4d7fec35baa01c41a4e9
root <lapique.philippe@gmail.com>
2025-02-28 10:13:51 +0000

    plouf

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,13 @@ void print_array(int *a, int n)
 {
 int b;
 	{
-		if (n <= '0')
+		if (n <= 0)
 		{
 		return;
 		}
-		for (b = '0'; b < n ; b++)
+		for (b = 0; b < n ; b++)
 		{
-		if ( b == '0')
+		if ( b == 0)
 		{
 		printf("%d", a[b]);
 		}
