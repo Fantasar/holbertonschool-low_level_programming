7f985d8d986095dad8080c4205b2d7a4aa436015
root <lapique.philippe@gmail.com>
2025-06-16 12:14:05 +0000

    Script pour defiler l'alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Printf "with proper grammar,
+ *		  but the outcome is a piece of art,"
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char dicos;
+
+for (dicos = 'a'; dicos <= 'z'; dicos++)
+	{
+	putchar(dicos);
+	}
+putchar('\n');
+return (0);
+}
