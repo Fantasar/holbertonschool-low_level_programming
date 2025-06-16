60d16f688820629c21b9b0826c355a338ff10462
root <lapique.philippe@gmail.com>
2025-06-16 13:29:58 +0000

    compte jusqu'a 9 sans printf

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Printf "with proper grammar,
+ *		  but the outcome is a piece of art,"
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
