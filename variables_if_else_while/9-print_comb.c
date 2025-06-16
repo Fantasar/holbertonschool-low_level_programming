5860663a91d206828d366aa778d2748173aef866
root <lapique.philippe@gmail.com>
2025-06-16 14:07:25 +0000

    script 9 suite de chiffre et espace

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Printf "with proper grammar,
+ *		  but the outcome is a piece of art,"
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int num;
+
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+{
+if (num < '9')
+putchar (',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
