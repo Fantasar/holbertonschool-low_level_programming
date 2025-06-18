6f9abed3195035d46bbfbb6de2acd104166eb031
root <lapique.philippe@gmail.com>
2025-06-18 13:00:36 +0000

    ajout du != pour ne pas avoir la colonne de zero

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,17 +14,20 @@ int b;
 int c;
 for (a = 0; a <= 9; a++)
 	{
-	_putchar('0');
 	for (b = 0; b <= 9; b++)
+		{
+		c = a*b;
+		if( b != 0)
 		{
 		_putchar(',');
 		_putchar(' ');
 
-		c = a * b;
 		if (c <= 9)
 		_putchar(' ');
-		else
+		}
+		if (c > 9)
 		_putchar((c / 10) + '0');
+
 		_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
