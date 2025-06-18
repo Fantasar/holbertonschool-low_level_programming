ed24019e4134b4f46992068e2d9de6cc4348c922
root <lapique.philippe@gmail.com>
2025-06-18 13:09:22 +0000

    addition de deux nombres

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - addition de deux entiers
+* @a: premier chiffre
+* @b: deuxieme chiffre
+* Return: le resultat
+*/
+
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
