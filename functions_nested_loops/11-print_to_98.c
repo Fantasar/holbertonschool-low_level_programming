df8cd2d94e9eaa6c24c7edbdbe446f80937520e8
root <lapique.philippe@gmail.com>
2025-06-19 12:31:56 +0000

    plouf

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+* print_to_98 - imprime les nombres dans l'ordre de n a 98
+* @n: valeurs de n dans la fonction
+*
+* Return: un vide
+*/
+
 void print_to_98(int n)
 {
-	if(n <= 98)
+int a;
+if (n < 98)
+{
+	for (a = n; a <= 98; a++)
 	{
-		for(n = 0; n < 98; n++)
-		_putchar(n);
+		if (a == 98)
+			{
+			printf("%d ", a);
+			}
+		else
+			{
+			printf("%d, ", a);
+			}
 	}
-	if (n > 98)
+	printf("\n");
+}
+else
+{
+	for (a = n; a >= 98; a--)
 	{
-	_putchar(n--);
+		if (a == 98)
+			{
+			printf("%d ", a);
+			}
+		else
+			{
+			printf("%d, ", a);
+			}
 	}
-
+	printf("\n");
+}
 }
 
 
