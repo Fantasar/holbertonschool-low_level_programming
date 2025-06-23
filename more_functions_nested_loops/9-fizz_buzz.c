70bf71cd481624d68c182249c96ca70228b642b2
root <lapique.philippe@gmail.com>
2025-06-23 13:14:55 +0000

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Enigme de FizzBuzz.
+ *
+ * Return: Les nombres de 1 a 100.
+ */
+
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			{
+			printf("FizzBuzz ");
+			}
+		else if (a % 5 == 0)
+			{
+			printf("Buzz ");
+			}
+		else if (a % 3 == 0)
+			{
+			printf("Fizz ");
+			}
+		else
+			{
+			printf("%d ", a);
+			}
+	}
+printf("\n");
+
+return (0);
+}
