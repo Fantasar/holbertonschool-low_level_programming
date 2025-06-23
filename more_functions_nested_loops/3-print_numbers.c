c3cfff3a0f0795d71eaa71704d900e8f95ea22e1
root <lapique.philippe@gmail.com>
2025-06-23 09:09:06 +0000

    fonction pour afficher les nombres

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - affiche les nombres de 0 a 9.
+ *
+ *
+ * Return: Les nombres de 0 a 9 dans la variable a.
+ */
+
+void print_numbers(void)
+{
+char a;
+for (a = 0; a <= 9; a++)
+	{
+	_putchar(a + '0');
+	_putchar('\n');
+	}
+
+}
