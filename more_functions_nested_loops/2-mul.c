1c053b54c9dd203d7434c6f4986e1b1c44861237
root <lapique.philippe@gmail.com>
2025-06-23 08:51:55 +0000

    Fonction pour multiplier deux entiers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplie deux entiers.
+ * @a: Une variable entier.
+ * @b: Une variable entier.
+ * Return: Le resultat mul
+ */
+
+int mul(int a, int b)
+
+{
+int mul = (a * b);
+return (mul);
+
+}
+
