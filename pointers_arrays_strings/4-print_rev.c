92c4da46846cca0a1e90175b0ab361f3d918d23c
root <lapique.philippe@gmail.com>
2025-06-24 10:30:17 +0000

    Script pour lire une phrases a l'envers stocker dans un tableau

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Imprime une phrase a l'envers.
+ *
+ * @s: Stocke la phrase a imprimer.
+ *
+ * Return: Une phrases en utilisant putchar.
+ */
+
+void print_rev(char *s)
+{
+int a = 0;
+int b;
+int c;
+while (s[a] != '\0')
+	{
+	a++;
+	}
+c = a;
+for (b = c - 1; b >= 0; b--)
+	{
+	_putchar(s[a]);
+	a--;
+	}
+_putchar('\n');
+}
