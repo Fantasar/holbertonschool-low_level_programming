232a0773a81f95c15c9715f2de03f74bdf7f5bdc
root <lapique.philippe@gmail.com>
2025-06-24 07:54:26 +0000

    script d'un pointeur

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Renvoie le chiffre 98 dans un pointeur.
+ *@n: Declaration d'un pointeur
+ * Return: Modifie le chiffre present dans le pointeur n.
+ */
+
+void reset_to_98(int *n)
+{
+int a;
+a = 98;
+*n = a;
+
+}
