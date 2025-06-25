c9be89530b0121d507db961961229ada798ad32c
root <lapique.philippe@gmail.com>
2025-06-25 08:09:15 +0000

    script qui compte a la moiter

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,9 +2,10 @@
 #include "main.h"
 
 /**
- * main - Enigme de FizzBuzz.
+ *puts_half - Affiche la moitier d'une series de nombres.
  *
- * Return: Les nombres de 1 a 100.
+ *@str: Un pointeur.
+ * Return: La moitier des nombres, de la serie.
  */
 
 void puts_half(char *str)
@@ -13,26 +14,26 @@ int a = 0;
 int b;
 int temp;
 
-while (str[a] != '\0')
+while (str[a] != '\0') /* calcul la longeur.*/
 	{
 	a++;
 	}
 
-if (a % 2 == 0)
-{
+if (a % 2 == 0) /*Si la longeur est paire.*/
+	{
 	b = a / 2;
-	for (temp = b; temp < a; b++)
-	{
+	for (temp = b; temp < a; temp++)
+		{
 		_putchar(str[temp]);
+		}
 	}
-}
-else
-{
-	b = (a - 1) / 2;
-	for (temp = b + 1; temp < a; temp--)
+else /* Si la longeur est impair.*/
 	{
-		_putchar(str[temp]);
+	b = (a - 1) / 2; /* Commence apres le milieu.*/
 	}
-}
+for (temp = b; temp < a; temp++)
+	{
+	_putchar(str[temp]);
+	}
 _putchar('\n');
 }
