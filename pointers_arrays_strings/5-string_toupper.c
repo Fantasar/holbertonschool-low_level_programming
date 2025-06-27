250b544b2c7f5e75a344f6aba9ed6cf56c512eea
root <lapique.philippe@gmail.com>
2025-06-27 09:12:57 +0000

    fonction pour inverser un caractere minuscule en majuscule

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * string_toupper - Imprime une phrase.
+ *
+ * @str: Le string en minuscule.
+ *
+ * Return: Une phrases en en majuscule;
+ *
+ */
+
+char *string_toupper(char *str)
+{
+/* Initialisation d'un pointeur sur la chaine str */
+char *ptr = str;
+
+while (*ptr != '\0')
+	{
+	if (*ptr >= 'a' && *ptr <= 'z')
+/* fonction toouper permet de modifier les caracteres*/
+		{
+		*ptr = toupper(*ptr);
+		}
+	ptr++;
+	}
+return (str);
+}
