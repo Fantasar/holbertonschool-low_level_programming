31a41f08e01ccf155c2ba361f95cf4925ba927d9
root <lapique.philippe@gmail.com>
2025-06-30 13:03:48 +0000

    Script qui permet d'afficher une chaine de caractere a partir d'un pointeur

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - Fonction qui va chercher le premier caractere de c dans s.
+ *
+ * @s: Un pointeur vers la variable a controler.
+ * @c: Le caractere qui va etre chercher dans *s.
+ * Return: return le debut de la chaine de caractere.
+ */
+
+char *_strchr(char *s, char c)
+{
+int temp;
+
+for (temp = 0; s[temp] != '\0'; temp++)
+	{
+	if (s[temp] == c)
+		{
+		return (&s[temp]);
+		}
+	}
+return (NULL);
+}
+
