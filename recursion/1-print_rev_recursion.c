6b1a278978784672b094fd21400aea454c36b771
root <lapique.philippe@gmail.com>
2025-07-01 13:11:12 +0000

    Programme qui ressort une phrase a l'envers

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Fonction qui retourne a l'envers.
+ *
+ * @s: Un pointeur vers la zone memoire a modifier.
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+	{
+	}
+else
+	{
+	_print_rev_recursion(s + 1);
+	}
+_putchar(*s);
+}
