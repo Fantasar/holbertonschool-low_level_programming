df632c771bfc1bea975e94a25f5fc2f773a74f51
root <lapique.philippe@gmail.com>
2025-07-02 13:52:05 +0000

    argv indique le nom des argument de la fonction

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Fonction principal.
+ *@argc: entier qui contient le nombre d'argument de la fonction.
+ *@argv: Tableaux qui contient le nom des argument de la fonction.
+ *Return: return 0 en cas de reussite.
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
