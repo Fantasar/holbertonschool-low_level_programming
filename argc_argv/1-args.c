e1297bb87724c2da40410595b33904cd28d10743
root <lapique.philippe@gmail.com>
2025-07-02 14:03:03 +0000

    args permet de compter le nombre d'arguments de la fonction main

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Fonction principal.
+ *@argc: Contient le nombre d'arguments dans la fonction.
+ *@argv: Contient un tablaux de chaine de caracter de la fonction.
+ *Return: return 0 en cas de reussite du programme.
+*/
+
+int main(int argc, char *argv[])
+
+{
+(void)*argv;
+
+printf("%d\n", argc);
+
+return (0);
+}
