9789826f44c0e175ef72d0688e0d614f4afd3a7b
root <lapique.philippe@gmail.com>
2025-07-02 12:49:44 +0000

    Fonction pour verifier un nombre premier

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,15 +2,43 @@
 #include "main.h"
 
 /**
- *_pow_recursion - Fonction qui va realiser une puissance.
- *
- * @x: Variable numero une.
- * @y: Variable numero deux.
- * Return: renvoie la puissance de x et y.
+ * boucle - Fonction qui va verifier les condition de n pour
+ *	    savoir si c'est un nombre premier.
+ * @n: Contient le chiffre mystere.
+ * @div: Contient 2 au debut de la premiere incremantation.
+ * Return: relance la boucle en ajoutant un +1 a 2.
+ */
+
+int boucle(int n, int div)
+{
+if (div >= n)
+	{
+	return (1);
+	}
+if (n % div == 0)
+	{
+	return (0);
+	}
+return (boucle(n, div + 1));
+}
+
+/**
+ * is_prime_number - Fonction qui va controler si n est deja 1.
+ * @n: Contient le chiffre mystere.
+ * Return: Appelles la recursion avec la fonction boucle
+ *	   et un multiple de 2.
  */
 
 int is_prime_number(int n)
 {
-if (n
+if (n <= 1)
+	{
+	return (0);
+	}
+else
+	{
+	return (boucle(n, 2));
+	}
+}
 
 
