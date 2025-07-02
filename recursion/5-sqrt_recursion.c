3c99edf5700b8d6f74bae1e2bd776759d7b6013d
root <lapique.philippe@gmail.com>
2025-07-02 09:44:26 +0000

    modification sur le parametre num

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,35 +3,41 @@
 
 /**
  * _sqrt_recursion - Fonction qui renvoie la racine carrer d'un nombre positifs
- * @num: La variable temporaire que l'on va multiplier pour chercher n.
  * @n: Le nombre initiales.
  * Return: La racine d'un nombre n si n est negatif
  * renvoie -1 si 0 renvoie 0.
  */
 
-int _sqrt_recursion(int n, int num)
+int _sqrt_recursion(int n)
 {
-
+static int num;
+static int boucle = 1;
+{
+if (boucle)
+	{
+	num = 0;
+	boucle = 0;
+	}
 if (n < 0)
 	{
+	boucle = 1;
 	return (-1);
 	}
-else if (n == 0)
+if (num * num == n)
 	{
-	return (0);
+	int plouf = num;
+
+	boucle = 1;
+	return (plouf);
 	}
-else if (num * num == n)
+if (num * num > n)
 	{
-	return (num);
-	}
-else if (num * num > n)
-	{
+	boucle = 1;
 	return (-1);
 	}
-else
-	{
-	return (_sqrt_recursion(n, num + 1));
-	}
+num++;
+return (_sqrt_recursion(n));
+}
 }
 
 
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_pow_recursion - Fonction qui va realiser une puissance.
+ *
+ * @x: Variable numero une.
+ * @y: Variable numero deux.
+ * Return: renvoie la puissance de x et y.
+ */
+
+int is_prime_number(int n)
+{
+if (n
+
+
