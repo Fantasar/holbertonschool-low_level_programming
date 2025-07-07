9d81b9143211c9823d26bbf1503025fc218387e3
root <lapique.philippe@gmail.com>
2025-07-07 09:38:53 +0000

    copie une phrases

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - copie deux string.
+ *
+ *@str: string a copier.
+ * Return: retourne dup l'emplacement de la copie.
+ */
+
+char *_strdup(char *str)
+{
+char *dup;
+int a;
+int b = 0;
+int memoire;
+
+if (str == NULL)
+	{
+	return (NULL);
+	}
+memoire = strlen(str) + 1;
+dup = malloc(memoire *sizeof(char));
+if (dup == NULL)
+	{
+	return (NULL);
+	}
+for (a = 0; str[a] != '\0'; a++)
+	{
+	dup[b] = str[a];
+	b++;
+	}
+dup[b] = '\0';
+return (dup);
+}
