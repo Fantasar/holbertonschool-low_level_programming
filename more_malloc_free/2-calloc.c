3cec40159c3891a455406113abebfd2914145632
root <lapique.philippe@gmail.com>
2025-07-08 13:48:53 +0000

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -4,13 +4,30 @@
 #include <string.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
+ *_calloc - calloc une fonctio.
+ * @nmemb: the address of memory to print
  * @size: the size of the memory to print
  *
- * Return: Nothing.
+ * Return: calloc.
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+int *calloc;
+unsigned int a;
 
+if (nmemb == 0 || size == 0)
+	{
+	return (NULL);
+	}
+calloc = malloc(nmemb * size);
+if (calloc == NULL)
+	{
+	return (NULL);
+	}
+for (a = 0; a < nmemb; a++)
+	{
+	calloc[a] = 0;
+	}
+return (calloc);
+}
