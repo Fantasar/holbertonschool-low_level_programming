2fec6c6e12b2a9a942e4344729547f58c0944766
root <lapique.philippe@gmail.com>
2025-07-08 09:07:31 +0000

    malloc return 98

---
+++ malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
