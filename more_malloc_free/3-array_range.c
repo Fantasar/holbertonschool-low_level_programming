775c040c5096d6bfb6355d88ad47ce5028277f05
root <lapique.philippe@gmail.com>
2025-07-09 08:29:17 +0000

    tableaux d'entier

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+int *array_range(int min, int max)
+{
+int *ptr;
+int a;
+int taille;
+if (min > max)
+	{
+	return (NULL);
+	}
+
+taille = max - min + 1;
+ptr = malloc(taille * sizeof(int));
+if (ptr == NULL)
+	{
+	return (NULL);
+	}
+for(a = 0; min < taille; a++)
+	{
+	ptr[a] = min + a;
+	}
+return (ptr);
+}
