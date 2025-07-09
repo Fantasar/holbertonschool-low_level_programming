97d7e45b35c56ecc28fca853697d52afb5f83645
root <lapique.philippe@gmail.com>
2025-07-09 14:03:55 +0000

    Modif

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,36 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
- * str_concat - Concatise deux phrase ensemble.
+ * str_concat - Concatise deux phrase ensembles.
  *@s1: la 1 ere phrase.
  *@s2: la 2 eme phrase.
- * Return: retourn la capoies des deux phrases..
+ * Return: retourn la copies des deux phrases..
  */
+
 char *str_concat(char *s1, char *s2)
 {
 char *str;
 int a, c, b, d, taille, taille2, plouf;
-int index = 0;
+taille = 0;
+taille2 = 0;
 
 if (s1 == NULL)
-	{
-	return ("");
-	}
+{
+return (NULL);
 for (c = 0; s1[c] != '\0'; c++)
 	{
 	taille++;
 	}
+}
 if (s2 == NULL)
-	{
-	return ("");
-	}
+{
+return (NULL);
 for (d = 0; s2[d] != '\0'; d++)
 	{
 	taille2++;
 	}
-plouf = taille + taille2 + 1;
-str = malloc(plouf *sizeof(char));
+}
+
+plouf = taille + taille2;
+
+str = (char *)malloc(sizeof(char) * (plouf + 1));
 
 if (str == NULL)
 	{
@@ -39,14 +44,12 @@ if (str == NULL)
 	}
 for (a = 0; a < taille; a++)
 	{
-	str[index] = s1[a];
-	index++;
+	str[a] = s1[a];
 	}
 for (b = 0; b < taille2; b++)
 	{
-	str[index] = s2[b];
-	index++;
+	str[a] = s2[b];
 	}
-str[index] = '\0';
+str[plouf] = '\0';
 return (str);
 }
