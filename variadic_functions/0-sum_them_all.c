ef59e7291fd160d1ae7278c39be93ceb7db08039
root <lapique.philippe@gmail.com>
2025-07-15 15:09:29 +0000

    Addition de variables constantes

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * main - check the code
- *
+ * sum_them_all - Additionne des fonction varibles
+ *@n: variables constantes.
  * Return: Always 0.
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
+unsigned int a;
+int resultat = 0;
 
+va_list args;
+va_start(args, n);
+
+if (n == 0)
+	{
+	return (0);
+	}
+for (a = 0; a < n; a++)
+	{
+	int arg = va_arg(args, unsigned int);
+
+	resultat += arg;
+	}
+va_end(args);
+return (resultat);
+}
