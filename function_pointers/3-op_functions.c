21504490429f408aecf38af3243ea59827ba619b
root <lapique.philippe@gmail.com>
2025-07-15 09:55:36 +0000

    Modification

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -3,58 +3,71 @@
 #include "3-calc.h"
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ *op_add - Addition de a et b
+ * @a: c'est un entier.
+ * @b: c'est un entier.
+ * Return: Le resultat.
  */
 
 int op_add(int a, int b)
 {
-
+return (a + b);
 }
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ *op_sub - Soustraction de a et b
+ * @a: c'est un entier.
+ * @b: c'est un entier.
+ * Return: Le resultat.
  */
+
 int op_sub(int a, int b)
 {
-
+return (a - b);
 }
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ *op_mul - Multiplication de a et b
+ * @a: c'est un entier.
+ * @b: c'est un entier.
+ * Return: Le resultat.
  */
+
 int op_mul(int a, int b)
 {
-
+return (a * b);
 }
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ *op_div - Division de a et b
+ * @a: c'est un entier.
+ * @b: c'est un entier.
+ * Return: Le resultat.
  */
+
 int op_div(int a, int b)
 {
-
+if (a || b = 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+return (a / b);
 }
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ *op_mod - Modulo de a et b
+ * @a: c'est un entier.
+ * @b: c'est un entier.
+ * Return: Le resultat.
  */
 
 int op_mod(int a, int b)
 {
-
+if (a || b = 0)
+	{
+	printf("Error\n");
+	exit(100);
+	}
+return (a % b);
 }
