d98cfb0e704cd1619188bfe18d8ed54995ff90d1
root <lapique.philippe@gmail.com>
2025-07-15 09:21:20 +0000

    Structure de la calculatrice

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+int op_add(int a, int b)
+{
+
+}
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int op_sub(int a, int b)
+{
+
+}
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int op_mul(int a, int b)
+{
+
+}
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int op_div(int a, int b)
+{
+
+}
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+int op_mod(int a, int b)
+{
+
+}
