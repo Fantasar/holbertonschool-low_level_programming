60732f88997707b5807228e44c35734fe137040f
root <lapique.philippe@gmail.com>
2025-07-16 12:06:20 +0000

    Modification Betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,20 +1,58 @@
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
- * print_numbers - Imprime des nombres.
- * @n: Nombres a imprimer.
- * @separator: signe de l'operateur
+ * print_arg - prints a single argument based on type
+ * @type: the type of the argument
+ * @args: the va_list to fetch from
  */
+void print_arg(char type, va_list args)
+{
+	char *s;
 
+	if (type == 'c')
+	printf("%c", va_arg(args, int));
+	else if (type == 'i')
+	printf("%d", va_arg(args, int));
+	else if (type == 'f')
+	printf("%f", va_arg(args, double));
+	else if (type == 's')
+	{
+	s = va_arg(args, char *);
+	if (s == NULL)
+		printf("(nil)");
+	else
+		printf("%s", s);
+	}
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed
+ */
 void print_all(const char * const format, ...)
 {
-
 va_list args;
-va_start(args, );
+int a = 0, premier_v = 1;
 
-*char arg = va_arg(args, char *)
+va_start(args, format);
 
-va_end(args);
+while (format && format[a] != '\0')
+	{
+	if (format[a] == 'c' || format[a] == 'i' ||
+	format[a] == 'f' || format[a] == 's')
+	{
+		if (!premier_v)
+		printf(", ");
+		else
+		premier_v = 0;
+
+		print_arg(format[a], args);
+	}
+	a++;
+	}
+
+	printf("\n");
+	va_end(args);
 }
