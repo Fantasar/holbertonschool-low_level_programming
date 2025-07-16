f52b8404c7936eeb5b69d635ce44e871160e0d7f
root <lapique.philippe@gmail.com>
2025-07-16 12:19:50 +0000

    Modification Betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,46 +1,82 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed
+ * print_char - Prints a char
+ * @ap: Argument pointer
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_integer - Prints an integer
+ * @ap: Argument pointer
+ */
+void print_integer(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - Prints a float
+ * @ap: Argument pointer
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - Prints a string
+ * @ap: Argument pointer
+ */
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (!s)
+	{
+		printf("(nil)");
+		return;
+	}
+		printf("%s", s);
+}
+
+/**
+ * print_all - Prints anything
+ * @format: Types of arguments passed to function
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0, first = 1;
-	char *str;
+	print_type types[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_list ap;
+	char *separator = "";
+	int i = 0;
+	int j = 0;
 
-	va_start(args, format);
-
+	va_start(ap, format);
 	while (format && format[i])
 	{
-		if (format[i] == 'c' || format[i] == 'i' ||
-		    format[i] == 'f' || format[i] == 's')
+		while (types[j].type)
 		{
-			else if (!first)
-				printf(", ");
-			else if (first)
-				first = 0;
-
-			else if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			else if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-			else if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			else if (format[i] == 's')
+			if (*types[j].type == format[i])
 			{
-				str = va_arg(args, char *);
-				if (str)
-					printf("%s", str);
-				else if (!str)
-					printf("(nil)");
+				printf("%s", separator);
+				types[j].f(ap);
+				separator = ", ";
 			}
+			++j;
 		}
-		i++;
+		j = 0;
+		++i;
 	}
 	printf("\n");
-	va_end(args);
+	va_end(ap);
 }
