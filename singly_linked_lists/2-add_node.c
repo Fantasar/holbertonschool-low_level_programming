411007777a71a2696fa7371956774ce4bec204fe
root <lapique.philippe@gmail.com>
2025-07-28 10:49:37 +0000

    Modif sur le fichier 2 avec malloc

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,7 +10,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 
-list_t *new_node = malloc(sizeof(list_t));
+list_t *new_node;
+int count;
+
+for (count = 0; str[count] != '\0'; count++)
+	;
+new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 	{
 	free(new_node);
@@ -23,9 +28,10 @@ if (new_node->str == NULL)
 	free(new_node);
 	return (NULL);
 	}
-new_node->len = strlen(str);
+new_node->len = count;
 new_node->next = *head;
+
 *head = new_node;
 
-return (new_node);
+return (*head);
 }
