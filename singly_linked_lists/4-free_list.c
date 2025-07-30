248db497cfdd6ebfaa5e2c24f7fba87f20723dea
root <lapique.philippe@gmail.com>
2025-07-30 14:13:10 +0000

    Modification sur free

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,5 +9,21 @@
 
 void free_list(list_t *head)
 {
+list_t *temp;
+
+if (head == NULL)
+	{
+	return;
+	}
+while (head->next != NULL)
+	{
+	temp = head->next;
+	free(head->str);
+	free(head);
+	head = temp;
+	}
+
+free(head->next);
+free(head->str);
 free(head);
 }
